95a9b7ab9c526bc75d95426a1d8067fb9f8b9a31
Giacomo <scampini.giacomo@gmail.com>
2026-02-07 10:54:46 +0100

    fix: remove unnecessary files transforming the output

--- backup.ts
+++ backup.ts
@@ -6,8 +6,6 @@
  * 3. Exports global definitions for Labels and Filters.
  */
 
-import processForAI from "./src/processForAI";
-
 const API_TOKEN = process.env.TODOIST_API_TOKEN;
 const SYNC_URL = "https://api.todoist.com/sync/v9/sync";
 
@@ -20,6 +18,7 @@ async function performBackup() {
   console.log("🚀 Starting Todoist download...");
 
   try {
+    // Downloading the json file.
     const response = await fetch(SYNC_URL, {
       method: "POST",
       headers: {
@@ -38,21 +37,25 @@ async function performBackup() {
       throw new Error(`API Error (${response.status}): ${errorText}`);
     }
 
+    // Taking the json from the response.
     const rawData = await response.json();
     console.log(
       `✅ Download complete. Processing ${rawData.items.length} items...`,
     );
 
-    const aiReadyData = processForAI(rawData);
+    // Process json to make it readable for AI.
+    // const aiReadyData = processForAI(rawData);
+    const aiReadyData = rawData;
 
+    // Saving the readable AI string to a file.
     const timestamp = new Date().toISOString().replace(/[:.]/g, "-");
     const cleanFilename = `todoist_ai_export_${timestamp}.txt`;
     await Bun.write(cleanFilename, JSON.stringify(aiReadyData, null, 2));
 
+    // logging
     console.log(`✅ Export successful!`);
     console.log(`📂 AI Context File: ${cleanFilename}`);
-
-    console.table(aiReadyData.meta.stats);
+    // console.table(aiReadyData.meta.stats);
   } catch (error) {
     console.error("❌ Backup failed:", error);
   }
